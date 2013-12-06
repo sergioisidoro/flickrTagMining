1b87d2a1dcebc29826d929befb380abc8e5f98d4
sergioisidoro <sergio.isidoro@ist.utl.pt>
2013-12-06 15:21:46 +0200

    Counting tags done

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,44 @@
+# -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+import math
+
+class Tag:
+    count = 1
+    weight = 0
+    
+    def setWeight(self, newWeight):
+        self.weight = newWeight
+        
+    def increment(self):
+        self.count +=1
 
 
+with open('dataset_small.txt', 'r') as content_file:
+        images = [line.strip().split(" ") for line in content_file]
+        
+
+numberOfImages = len(images);
+tagDictionary = {}
+    
+    
+for image in images:
+    for tag in image:
+        if tag in tagDictionary.keys():
+            tagDictionary[tag].increment()
+        else:
+            tagDictionary[tag] = Tag()
+            
+#DEBUG
+#print(tagDictionary)
+
+numberOfTags = len(tagDictionary)
+
+for tag in tagDictionary:
+    tagDictionary[tag].setWeight(math.log(numberOfImages) - math.log(tagDictionary[tag].count))
+    
+    #DEBUG
+    #print (tag + " ")
+    #print (tagDictionary[tag].weight)
+#DEBUG
+#print(numberOfTags)
